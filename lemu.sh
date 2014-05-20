b134b64704705122fdd0a5422fe3f2ed952b5928
Guilherme de Moraes Alvarez <guilherme@moraesalvarez.com>
2014-05-20 19:07:28 -0300

    initial
    e

---
+++ lemu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+zip lemu.nw index.html corpo.css package.json jquery.min.js main.js OpenSans.ttf lemu.db config.html
+
+clear
+nw lemu.nw
---
+++ main.js
@@ -0,0 +1,73 @@
+////////////////////////////////////
+/// vars and requires
+////////////////////////////////////
+var fs = require('fs');
+var sys = require('sys')
+var exec = require('child_process').exec;
+var dir = '/';
+
+list = document.getElementById('list');
+/////////////////////////////////
+// get config from localstorage
+///////////////
+var maqstr = '';
+var extstr = '';
+var exestr = '';
+if (localStorage.maqstr != null) maqstr = localStorage.maqstr;
+if (localStorage.extstr != null) extstr = localStorage.extstr;
+if (localStorage.exestr != null) exestr = localStorage.exestr;
+var maq = maqstr.split(';');
+var ext = extstr.split(';');
+var exe = exestr.split(';');
+
+var dirstr='';
+if (localStorage.dir != null) dir = localStorage.dir;
+if (localStorage.dirstr != null) dirstr = localStorage.dirstr;
+dirs=dirstr.split(';');
+////////////////////////////////////
+/// startup
+////////////////////////////////////
+list.innerHTML='carregando...';
+
+fs.readdir(dir, files_result);
+
+ldirs=document.getElementById('dirs');
+for (i=0;i<dirs.length;i++) {
+	ledir=dirs[i];
+	ledirnameArr=dirs[i].split('/');
+	ledirname=ledirnameArr[ledirnameArr.length-2]+'/'+ledirnameArr[ledirnameArr.length-1];
+	ldirs.innerHTML=ldirs.innerHTML+"<a class='button' href='javascript:void(0)' onclick='changedir(\""+ledir+"\")'>"+ledirname+"</a>";
+}
+////////////////////////////////////
+/// callbacks
+////////////////////////////////////
+function files_result (err, files) {
+	if (err) { //throw err;
+		files=[]; };
+	list.innerHTML='';
+	//console.log("files: " + files);
+	len = files.length;
+	for (i=0;i < len;i++) {		
+		if (ext.indexOf(files[i].substr(files[i].length-3)) > -1)		
+			list.innerHTML=list.innerHTML+
+			"<a class='buttond' href='javascript:void(0)' onclick='launch(\""+files[i]+"\")'>"+files[i]+"</a>\n";
+	};
+}
+
+function puts(error, stdout, stderr) { console.log(stdout) }
+
+////////////////////////////////////
+/// proper functions
+////////////////////////////////////
+function launch(filename){
+	console.log('launching '+filename);
+	emu_exec = 'echo';
+	emu_exec = exe[ext.indexOf(filename.substr(filename.length-3))];
+	//console.log(filename+" --> "+emu_exec);
+	exec(emu_exec+" \""+dir+"/"+filename+"\"", puts);
+}
+
+function changedir(ledir){
+	localStorage.dir=ledir;
+	location.reload();
+}
